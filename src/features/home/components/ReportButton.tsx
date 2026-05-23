8827400d088aad3bebff915cdc038ef0e01cbc31
hammmmmmsu <hamsj3808@gmail.com>
2026-05-23 14:12:31 +0900

    feat: ReportButton 아이콘 추가

--- src/features/home/components/ReportButton.tsx
+++ src/features/home/components/ReportButton.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Pressable, Text, View } from "react-native";
+import CallIcon from "@/features/home/assets/svg/Phone.svg";
 
 interface ReportButtonProps {
     onPress?: () => void;
@@ -9,12 +10,11 @@ export default function ReportButton({ onPress }: ReportButtonProps) {
     return (
         <Pressable
             onPress={onPress}
-            className="flex-row items-center justify-center bg-primary rounded-xl py-3 mt-3"
+            className="flex-row items-center justify-center bg-primary rounded-xl py-3 mt-3 gap-1"
         >
-            {/* 전화 아이콘 자리 (이미지 에셋 추후 추가) */}
-            <View className="w-4 h-4 rounded-sm bg-bk/70 mr-2" />
+            <CallIcon width={20} height={20} className="color-gr700" />
 
-            <Text className="text-bk text-sm font-bold">제보하기</Text>
+            <Text className="text-bk text-md font-bold">제보하기</Text>
         </Pressable>
     );
 }
